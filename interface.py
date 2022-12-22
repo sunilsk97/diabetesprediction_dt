ede8f4c480cd74f0a78e1f36729b81c11cc8396b
Sunil Kshirsagar <111547921+sunilsk97@users.noreply.github.com>
2022-12-22 19:01:26 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -52,6 +52,7 @@ def get_pred_result():
         return render_template("result.html", pred=pred_result)
 
    
-if __name__ =="__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
